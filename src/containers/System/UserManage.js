6818a2e1bb8e7672bbc156efdafbbdda03647505
Cohan Le <vietle632@gmail.com>
2023-11-10 20:30:37 +0700

    1110 - CNU

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,11 +3,13 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
 import { getAllUsers } from "../../services/userService";
+import ModalUser from "./ModalUser";
 class UserManage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrayUsers: [],
+      isOpenModalUser: false,
     };
   }
 
@@ -20,12 +22,37 @@ class UserManage extends Component {
     }
   }
 
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser: true,
+    });
+  };
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModalUser: !this.state.isOpenModalUser,
+    });
+  };
+
   render() {
     let arrayUsers = this.state.arrayUsers;
+    console.log(arrayUsers);
     return (
       <div className="users-container">
+        <ModalUser
+          isOpen={this.state.isOpenModalUser}
+          toggleFromParent={this.toggleUserModal}
+          test={"abs"}
+        />
         <div className="title text-center"> Manage users with Cohan</div>
-
+        <div className="mx-1">
+          <button
+            className="btn btn-primary px-3"
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i className="fa fa-plus mx-1"></i>
+            Add new user
+          </button>
+        </div>
         <div className="users-table mt-3 mx-1"></div>
         <table>
           <tr>
@@ -47,10 +74,10 @@ class UserManage extends Component {
                     <td>{item.address}</td>
                     <td>
                       <button className="btn-edit">
-                        <i class="fa fa-pencil"></i>{" "}
+                        <i className="fa fa-pencil"></i>
                       </button>
                       <button className="btn-delete">
-                        <i class="fa fa-trash"></i>
+                        <i className="fa fa-trash"></i>
                       </button>
                     </td>
                   </tr>
